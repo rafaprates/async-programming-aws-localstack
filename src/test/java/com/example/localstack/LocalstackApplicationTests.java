a3700d3ffd5dfe8602a2bdee6f8f274b807bad5e
Rafael Cardoso <rafaelbprates@gmail.com>
2023-07-17 12:26:07 -0300

    chore: basic spring boot setup with H2 database

---
+++ src/test/java/com/example/localstack/LocalstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.localstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
