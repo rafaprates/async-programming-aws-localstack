c1ea9c9d134e4f022a3e301da63b82563e8f712e
Rafael Cardoso <rafaelbprates@gmail.com>
2023-07-20 16:43:50 -0300

    fix(docker): subscribe to topic on localstack initialization

--- aws-init.sh
+++ aws-init.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+cat "Creating topic..."
 awslocal sns create-topic --name contratacao
+
+cat "Creating queues..."
 awslocal sqs create-queue --queue-name consulta-cep-queue
 awslocal sqs create-queue --queue-name consulta-cpf-queue
+
+cat "Subscribing queues to topic..."
+awslocal sns subscribe --topic-arn arn:aws:sns:us-east-1:000000000000:contratacao --protocol sqs --notification-endpoint arn:aws:sqs:us-east-1:000000000000:consulta-cep-queue
+awslocal sns subscribe --topic-arn arn:aws:sns:us-east-1:000000000000:contratacao --protocol sqs --notification-endpoint arn:aws:sqs:us-east-1:000000000000:consulta-cpf-queue
